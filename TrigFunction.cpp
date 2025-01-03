0f5a0b9ebe79fb05dffdd2a8efa4029dc8640d4f
dumooon <nikolaenkodmitrij2004@gmail.com>
2025-01-03 15:28:13 +0200

    Finalized FuncA

--- TrigFunction.cpp
+++ TrigFunction.cpp
@@ -1,6 +1,13 @@
 #include "TrigFunction.h"
+#include <cmath>
 
-// Initial implementation: FuncA returns 0.
-double TrigFunction::FuncA() {
-    return 0;
+// Implementation of FuncA:
+// Calculates the sum of the first n elements of the arctan series.
+// Param n: Number of elements in the series to calculate.
+double TrigFunction::FuncA(int n) {
+    double result = 0.0;
+    for (int i = 0; i < n; ++i) {
+        result += (std::pow(-1, i) * std::pow(x, 2 * i + 1)) / (2 * i + 1);
+    }
+    return result;
 }
