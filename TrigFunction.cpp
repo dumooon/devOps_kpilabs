32f75108aac42fc037a20fa92f3931da4cb6f52b
dumooon <nikolaenkodmitrij2004@gmail.com>
2025-01-03 15:26:56 +0200

    hotfix

--- TrigFunction.cpp
+++ TrigFunction.cpp
@@ -1,6 +1,12 @@
 #include "TrigFunction.h"
+#include <cmath>
 
-// Initial implementation: FuncA returns 0.
+// Implementation of FuncA: calculates sum of first 3 elements of the series
 double TrigFunction::FuncA() {
-    return 0;
+    int n = 3;
+    double result = 0.0;
+    for (int i = 0; i < n; ++i) {
+        result += (std::pow(-1, i) * std::pow(x, 2 * i + 1)) / (2 * i + 1);
+    }
+    return result;
 }
